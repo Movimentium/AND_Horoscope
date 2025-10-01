9410c30d986a48283141ef6f7870ec25b77be44a
Miguel Gallego <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-01 10:52:41 +0200

    SessionManager.kt done

--- app/src/main/java/com/miguel_gallego/and_horoscope/DetailActivity.kt
+++ app/src/main/java/com/miguel_gallego/and_horoscope/DetailActivity.kt
@@ -1,16 +1,30 @@
 package com.miguel_gallego.and_horoscope
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import org.w3c.dom.Text
+
 class DetailActivity : AppCompatActivity() {
+
+    lateinit var tvSignName: TextView
+    lateinit var tvSingDates: TextView
+    lateinit var imgVwSign: ImageView
+
+    lateinit var sing: ZodiacSing
+    //lateinit var session: SessionManager // WTF Is This??
+    lateinit var menuFavorite: MenuItem
+    var isFavorite = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -18,5 +32,16 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Views
+        imgVwSign = findViewById(R.id.imgVwSign)
+        tvSignName = findViewById(R.id.tvSignName)
+        tvSingDates = findViewById(R.id.tvSignDates)
+
+        // session = SessionManager(this)  // WTF Is This??
+
+        val id = intent.getStringExtra("HOROSCOPE_ID")!! // WTF Is This?
+
+        isFavorite = session
     }
 }
---
+++ app/src/main/java/com/miguel_gallego/and_horoscope/utils/SessionManager.kt
@@ -0,0 +1,27 @@
+package com.miguel_gallego.and_horoscope.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.miguel_gallego.and_horoscope.ZodiacSing
+
+class SessionManager(context: Context) {
+
+    private var sharedPrefs: SharedPreferences = context.getSharedPreferences("horoscope_session",
+        Context.MODE_PRIVATE)
+
+    private val keyFavoriteSingId = "FAVORITE_HOROSCOPE_ID"
+
+    fun setFavorite(zodiacSingId: String) {
+        val prefsEditor = sharedPrefs.edit()
+        prefsEditor.putString(keyFavoriteSingId, zodiacSingId)
+        prefsEditor.apply()
+    }
+
+    fun getFavorite(): String {
+        return sharedPrefs.getString(keyFavoriteSingId, "")!! // key, defaultValue
+    }
+
+    fun isFavorite(zodiacSingId: String): Boolean {
+        return zodiacSingId == getFavorite()
+    }
+}
