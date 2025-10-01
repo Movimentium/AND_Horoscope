29fad33652fde9228597d983f806e8a50d6102f7
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-01 20:12:46 +0200

    MainActivity.kt moved to activities

--- app/src/main/java/com/miguel_gallego/and_horoscope/DetailActivity.kt
+++ app/src/main/java/com/miguel_gallego/and_horoscope/DetailActivity.kt
@@ -11,37 +11,3 @@ import androidx.core.view.WindowInsetsCompat
 
 import org.w3c.dom.Text
 
-class DetailActivity : AppCompatActivity() {
-
-    lateinit var tvSignName: TextView
-    lateinit var tvSingDates: TextView
-    lateinit var imgVwSign: ImageView
-
-    lateinit var sing: ZodiacSing
-    //lateinit var session: SessionManager // WTF Is This??
-    lateinit var menuFavorite: MenuItem
-    var isFavorite = false
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        // Views
-        imgVwSign = findViewById(R.id.imgVwSign)
-        tvSignName = findViewById(R.id.tvSignName)
-        tvSingDates = findViewById(R.id.tvSignDates)
-
-        // session = SessionManager(this)  // WTF Is This??
-
-        val id = intent.getStringExtra("HOROSCOPE_ID")!! // WTF Is This?
-
-        //isFavorite = session
-    }
-}
---
+++ app/src/main/java/com/miguel_gallego/and_horoscope/activities/MainActivity.kt
@@ -0,0 +1,2 @@
+package com.miguel_gallego.and_horoscope.activities
+
